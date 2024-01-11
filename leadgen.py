b535e38796d99de34e112ccfdb51a287935c953e
arseniy <inq134@gmail.com>
2024-01-11 17:59:22 +0300

    demo amocrm

---
+++ leadgen.py
@@ -0,0 +1,291 @@
+import dotenv
+import jwt
+import requests
+from datetime import datetime
+import time
+import logging
+from requests.exceptions import JSONDecodeError
+from dotenv import load_dotenv
+import os
+
+dotenv_path = os.path.join('', '.env')
+load_dotenv(dotenv_path=dotenv_path)
+load_dotenv()
+
+
+subdomain = os.getenv("AMOCRM_SUBDOMAIN")
+client_id = os.getenv("AMOCRM_CLIENT_ID")
+client_secret = os.getenv("AMOCRM_CLIENT_SECRET")
+redirect_uri = os.getenv("AMOCRM_REDIRECT_URL")
+secret_code = "def50200bbb4b474f9566d4fb3a045677918cf906acfb0341bdff56eee9689610923bb9b9e771a1551621ab799fc99e34f34018fbfdd748383561ad6db4dfd28296e5920c48756aa02ed509924ed10b0f0242dbac22ac77e3330e5310288dbe64963bcf4a11df0fb850dc2e147e642a1b01fd26fd413de01dbd49fa7a00399915fe649b5d7efedf7eb40acbce6a87789479511671f0067a6996d1361bc4d92632c0264e31eca87a2143e61465ef02be8fdef372245de304aa7089cc9d3ca0c66c68a169a85b51ad282e71ed93c6a95b9c503ed0e7f25ec17a874e7a327f9b7d4edd72c8ddccb37da5c771394722f8a366cc5c8b00c53ec5ce573e8f2d483677be932782ad34c5921fe8830ab41f63579f90fdd7f8a293568e0ba365935dd40ff45ddfdf085c62c32ccd4eaf6cab20e28412c9b76cdf82ba21cb0551f4269311ff15d9f313131928477bb83fb59bc328ca8d19760813f56eba3da8cd77aa5740d8fce2b1463f90c26040f1101b14de9f57210dfd51672200e60855721f57f18a36e75565b75b7e0a2beda399298099c3d31e49e8223dc2923349fca8b986eeba0970177b2f9b1f20e0c24b362b7f3de0002b1f6148ecbfc4275f9523c951d21630dc6eeb7f98ec718cfafa14b38176420097227e6b075f60808d1fff6ecd7462ca634e633c3b978bc84879d6f74"
+
+def _is_expire(token: str):
+    token_data = jwt.decode(token, options={"verify_signature": False})
+    exp = datetime.utcfromtimestamp(token_data["exp"])
+    now = datetime.utcnow()
+    return now >= exp
+
+
+def _save_tokens(access_token: str, refresh_token: str):
+    # Записываем в ключи .env
+    os.environ["AMOCRM_ACCESS_TOKEN"] = access_token
+    os.environ["AMOCRM_REFRESH_TOKEN"] = refresh_token
+    dotenv.set_key(dotenv_path, "AMOCRM_ACCESS_TOKEN", os.environ["AMOCRM_ACCESS_TOKEN"])
+    dotenv.set_key(dotenv_path, "AMOCRM_REFRESH_TOKEN", os.environ["AMOCRM_REFRESH_TOKEN"])
+
+
+def _get_refresh_token():
+    return os.getenv("AMOCRM_REFRESH_TOKEN")
+
+
+def _get_access_token():
+    return os.getenv("AMOCRM_ACCESS_TOKEN")
+
+def _get_new_tokens():
+    data = {
+            "client_id": client_id,
+            "client_secret": client_secret,
+            "grant_type": "refresh_token",
+            "refresh_token": _get_refresh_token(),
+            "redirect_uri": redirect_uri
+    }
+    response = requests.post("https://{}.amocrm.ru/oauth2/access_token".format(subdomain), json=data).json()
+    access_token = response["access_token"]
+    refresh_token = response["refresh_token"]
+    _save_tokens(access_token, refresh_token)
+
+def add_unsorted_lead(self, **kwargs):
+    data = [
+        {
+            "source_name": kwargs.get("lead_name"),
+            "source_uid": "1",
+            "_embedded": {
+                "leads": [
+                    {
+                        "name": kwargs.get("form_lead_name"),
+                        "custom_fields_values": [
+                            {
+                                # utm_content
+                                "field_id": 92773,
+                                "values": [
+                                    {
+                                        "value": kwargs.get("form_utm_content")
+                                    }
+                                ]
+                            },
+                            {
+                                # utm_medium
+                                "field_id": 92775,
+                                "values": [
+                                    {
+                                        "value": kwargs.get("form_utm_medium")
+                                    }
+                                ]
+                            },
+                            {
+                                # utm_campaign
+                                "field_id": 92777,
+                                "values": [
+                                    {
+                                        "value": kwargs.get("form_utm_campaign")
+                                    }
+                                ]
+                            },
+                            {
+                                # utm_source
+                                "field_id": 92779,
+                                "values": [
+                                    {
+                                        "value": kwargs.get("form_utm_source")
+                                    }
+                                ]
+                            },
+                            {
+                                # utm_term
+                                "field_id": 92781,
+                                "values": [
+                                    {
+                                        "value": kwargs.get("form_utm_term")
+                                    }
+                                ]
+                            },
+                            {
+                                # _ym_uid
+                                "field_id": 92801,
+                                "values": [
+                                    {
+                                        "value": kwargs.get("form_ym_uid")
+                                    }
+                                ]
+                            },
+                            {
+                                # _ym_counter
+                                "field_id": 92803,
+                                "values": [
+                                    {
+                                        "value": kwargs.get("form_ym_counter")
+                                    }
+                                ]
+                            },
+                            {
+                                # Страница
+                                "field_id": 982883,
+                                "values": [
+                                    {
+                                        "value": kwargs.get("form_page_name")
+                                    }
+                                ]
+                            },
+                            {
+                                # Форма
+                                "field_id": 1923211,
+                                "values": [
+                                    {
+                                        "value": kwargs.get("form_form_name")
+                                    }
+                                ]
+                            },
+
+                        ],
+                        "_embedded": {
+                            "tags": [
+                                {
+                                    "name": "Сайт"
+                                }
+                            ]
+                        }
+                    }
+                ],
+                "contacts": [
+                    {
+                        "name": kwargs.get("form_lead_name"),
+                        # "first_name": "first_name justcode_v2",
+                        # "last_name": "surname justcode_v2",
+                        "custom_fields_values": [
+                            {
+                                "field_code": "PHONE",
+                                "values": [
+                                    {
+                                        "value": kwargs.get("form_phone")
+                                    }
+                                ]
+                            },
+                            {
+                                "field_id": 1925337,
+                                "values": [
+                                    {
+                                        "value": kwargs.get("message")
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ],
+                # "companies": [
+                #     # {
+                #     #     "name": "ОАО Коспромсервис"
+                #     # }
+                # ]
+            },
+            "metadata": {
+                "ip": kwargs.get("ip_address"),
+                "form_id": "test",
+                "form_sent_at": int(time.time()),
+                "form_name": kwargs.get("form_form_name"),
+                "form_page": kwargs.get("form_page_name"),
+                "referer": redirect_uri
+            }
+        }
+    ]
+
+    response = self._base_request(endpoint="/api/v4/leads/unsorted/forms", type="post", data=data)
+
+    lead_id = response.get('_embedded').get('unsorted')[0].get("_embedded").get("leads")[0].get("id")
+
+    # Ну или получать вот так :)
+    # for key, value in response.items():
+    #     if key == "_embedded":
+    # for item_key, item_value in test.items():
+    #     print(item_key, " ", item_value, "\n")
+    #     for k, v in item_value[0].items():
+    #         if k == "_embedded":
+    #             for x, y in v.items():
+    #                 if x == "leads":
+    #                     for a, b in y[0].items():
+    #                         if a == "id":
+    #                             return b
+    return lead_id
+
+
+class AmoCRMWrapper:
+    def init_oauth2(self):
+        if _get_access_token()== True and _is_expire(_get_access_token()):
+            print('token exists')
+            _get_new_tokens()
+            data = {
+                "client_id": client_id,
+                "client_secret": client_secret,
+                "grant_type": 'refresh_token',
+                "refresh_token": _get_refresh_token(),
+                "redirect_uri": redirect_uri
+            }
+            response = requests.post("https://{}.amocrm.ru/oauth2/access_token".format(subdomain), json=data).json()
+            print(response)
+            access_token = response["access_token"]
+            refresh_token = response["refresh_token"]
+            print(access_token)
+            print(refresh_token)
+            _save_tokens(access_token, refresh_token)
+        else:
+            data = {
+                "client_id": client_id,
+                "client_secret": client_secret,
+                "grant_type": "authorization_code",
+                "code": secret_code,
+                "redirect_uri": redirect_uri
+            }
+            response = requests.post("https://{}.amocrm.ru/oauth2/access_token".format(subdomain), json=data).json()
+            print(response)
+            access_token = response["access_token"]
+            refresh_token = response["refresh_token"]
+            print(access_token)
+            print(refresh_token)
+            _save_tokens(access_token, refresh_token)
+    def _base_request(self, **kwargs):
+        if _is_expire(_get_access_token()):
+            _get_new_tokens()
+
+        access_token = "Bearer " + _get_access_token()
+        headers = {"Authorization": access_token}
+        req_type = kwargs.get("type")
+        response = ""
+        if req_type == "get":
+            try:
+                response = requests.get("https://{}.amocrm.ru{}".format(
+                    subdomain, kwargs.get("endpoint")), headers=headers).json()
+            except JSONDecodeError as e:
+                logging.exception(e)
+        elif req_type == "get_param":
+            url = "https://{}.amocrm.ru{}?{}".format(
+                subdomain,
+                kwargs.get("endpoint"), kwargs.get("parameters"))
+            response = requests.get(str(url), headers=headers).json()
+        elif req_type == "post":
+            response = requests.post("https://{}.amocrm.ru{}".format(
+                subdomain,
+                kwargs.get("endpoint")), headers=headers, json=kwargs.get("data")).json()
+        return response
+
+
+amocrm_wrapper_1 = AmoCRMWrapper()
+amocrm_wrapper_1.init_oauth2()
+while True:
+    time.sleep(5)
+    if _is_expire(_get_access_token()):
+        _get_new_tokens()
+        print('token expired')
+    else:
+        _get_new_tokens()
+        print('token is alive')
+
+
--- test.py
+++ test.py
@@ -16,7 +16,7 @@ import openai
 from objects_creator import create_user_msg, create_ai_msg
 import utils
 from apps.home.models import ChatUser
-
+import threading
 
 app = Flask(__name__)
 sales_agent_manager = SalesAgentManager()
@@ -25,15 +25,7 @@ RECORDINGS_DIR = "recordings"
 
 # OpenAi API key
 openai.api_key = utils.openai_api_key
-token='$2y$10$TwWFjsbOidKKqGxF0F35c.SWtnFpkLAWMaqDZcSXo7kM7BTumz2p.'
-
-# handle audio messages
-def handle_audio_message(audio_id):
-    audio_url = get_media_url(audio_id)
-    audio_bytes = download_media_file(audio_url)
-    audio_data = convert_audio_bytes(audio_bytes)
-    audio_text = recognize_audio(audio_data)
-    return audio_text
+token = '$2y$10$TwWFjsbOidKKqGxF0F35c.SWtnFpkLAWMaqDZcSXo7kM7BTumz2p.'
 
 
 # make request to OpenAI
@@ -49,7 +41,8 @@ def make_openai_request(message, from_number):
     sales_agent = sales_agent_manager.get_sales_agent(from_number)
     sales_agent.human_step(message)
     sales_agent.determine_conversation_stage()
-    user, created = ChatUser.objects.get_or_create(user_name=from_number, user_id=from_number, messenger='Insta/whatsapp')
+    user, created = ChatUser.objects.get_or_create(user_name=from_number, user_id=from_number,
+                                                   messenger='Insta/whatsapp')
     create_user_msg(user, message)
     ai_answer = sales_agent.step()
     utils.write_to_history_assistant(from_number, ai_answer)
@@ -57,67 +50,47 @@ def make_openai_request(message, from_number):
     create_ai_msg(user, ai_answer, ai_prefix=ai_prefix)
     return ai_answer
 
-# handle incoming webhook messages
-def handle_message(request):
-    print('MESSAGE DETECTED')
-    body = request.get_json()
-    print(f"request body: {body}")
-
-    data = request.get_json()
-    message_data = data.get('data', [])[0].get('message', {})
-    chat_data = data.get('data', [])[0].get('chat', {})
-    # Извлекаем текст сообщения
-    text = message_data.get('text', 'No text found')
-    user_id= chat_data.get('id', 'No id found')
-    msg_status = data.get('data', [])[0].get('side', {})
-    print(f'status message: \n {msg_status}')
-    if msg_status == 'out':
-        return '500'
-        print('YOU MUST NEVER SEE THIS YOU MUST NEVER SEE THIS YOU MUST NEVER SEE THIS YOU MUST NEVER SEE THIS YOU MUST NEVER SEE THIS ')
-    print("Received message:", text)
-    answer_to_chat(make_openai_request(text, user_id), user_id)
-    return '200'
-
-
 
 def answer_to_chat(text, user_id):
     url = f'https://api.chatapp.online/v1/licenses/43053/messengers/instagram/chats/{user_id}/messages/text'
     payload = {
-        "text":text,
+        "text": text,
     }
     headers = {
-      'Authorization': token,
-      'Content-Type': 'application/json'
+        'Authorization': token,
+        'Content-Type': 'application/json'
     }
     response = requests.request('POST', url, headers=headers, json=payload)
     print(response.json())
+
+
+def process_message(text, user_id):
+    answer = make_openai_request(text, user_id)
+    answer_to_chat(answer, user_id)
+
 
 # Sets homepage endpoint and welcome message
 @app.route("/", methods=["GET", "POST"])
 def home():
     print('MESSAGE DETECTED')
-    body = request.get_json()
-    print(f"request body: {body}")
-
     data = request.get_json()
     message_data = data.get('data', [])[0].get('message', {})
     chat_data = data.get('data', [])[0].get('chat', {})
     # Извлекаем текст сообщения
     text = message_data.get('text', 'No text found')
-    user_id= chat_data.get('id', 'No id found')
+    user_id = chat_data.get('id', 'No id found')
     msg_status = data.get('data', [])[0].get('side', {})
     print(f'status message: \n {msg_status}')
     if msg_status == 'in':
-        answer = make_openai_request(text, user_id)
-        answer_to_chat(answer, user_id)
-        return '200'
+        # Start a new thread to process the message asynchronously
+        threading.Thread(target=process_message, args=(text, user_id)).start()
     return '200'
 
 
-if __name__ == "__main__":
-    # ssl_cert_path = "/etc/letsencrypt/live/guruai.store/fullchain.pem"
-    # ssl_key_path = "/etc/letsencrypt/live/guruai.store/privkey.pem"
+# @app.route("/callback", methods=["GET", "POST"])
+# def callback():
+#     print(request.get_json())
+#     return '200'
 
-    # ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-    # ssl_context.load_cert_chain(ssl_cert_path, ssl_key_path)
+if __name__ == "__main__":
     app.run(debug=True, use_reloader=True, host='0.0.0.0')
